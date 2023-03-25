0c44c874a6cd6487eefa273583fe37a1cf980627
MikePio <michelepiopilla@gmail.com>
2023-03-26 00:50:23 +0100

    Exercise completed without loop for and bonus

--- script.js
+++ script.js
@@ -11,6 +11,77 @@ Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di 
 
 console.log('test');
 
+const images = [
+  'img/01.webp',
+  'img/02.webp',
+  'img/03.webp',
+  'img/04.webp',
+  'img/05.webp'
+]
+
+const buttonUp = document.querySelector('.fa-chevron-up');
+const buttonDown = document.querySelector('.fa-chevron-down');
+let containerImages = document.getElementById('container-images');
+
+console.log(images);
+
+containerImages.innerHTML =  `<img src="${images[0]}" alt="img">`;
+let c = 0;
+
+buttonUp.addEventListener('click', function(){
+  // * soluzione 1 senza loop
+  if (c > 0){
+    c = c - 1;
+    containerImages.innerHTML =  `<img src="${images[c]}" alt="img">`;
+    console.log(c);
+  }  
+  else if(c = -1){
+    c = 4;
+    containerImages.innerHTML =  `<img src="${images[c]}" alt="img">`;
+    console.log(c);
+  }
+  // * OPPURE AL POSTO DI  else if(c = -1){
+  // else{
+  //   c = 4;
+  //   containerImages.innerHTML =  `<img src="${images[c]}" alt="img">`;
+  //   console.log(c);
+  // }
+
+});
+
+buttonDown.addEventListener('click', function(){
+  // * soluzione 1 senza loop
+  if (c < 4){
+    c = c + 1;
+    containerImages.innerHTML =  `<img src="${images[c]}" alt="img">`;
+    console.log(c);
+  }  
+  else if(c = 4){
+    c = 0;
+    containerImages.innerHTML =  `<img src="${images[c]}" alt="img">`;
+    console.log(c);
+  }
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
